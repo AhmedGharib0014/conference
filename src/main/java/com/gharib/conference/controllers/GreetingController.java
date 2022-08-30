8004ecaba03231f1dbb83e5ecd2d04e79dbbb887
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-08-30 22:28:33 +0200

    add thymeleaf config.. SpringResourceTemplate, TemplateEngine and ViewResolver

--- src/main/java/com/gharib/conference/controllers/GreetingController.java
+++ src/main/java/com/gharib/conference/controllers/GreetingController.java
@@ -3,17 +3,18 @@ package com.gharib.conference.controllers;
 
 import java.util.Map;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 @Controller
-@RequestMapping("/conference")
+@RequestMapping()
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(Map<String, Object> model){
+    public String greeting(Map<String,String> model){
         model.put("message","hi guest");
         return "index";
     }
